757192b9bd1184669f7bec719e7d817f7d9ff030
kotha32 <sumanthkotha32@gmail.com>
2024-11-14 12:28:49 +0530

    Added the inspection usage in xml

--- srv/sat.cds
+++ srv/sat.cds
@@ -1,63 +1,79 @@
 using { API_INSPECTIONLOT_SRV as inspectionLots } from './external/API_INSPECTIONLOT_SRV';
-using {com.satinfotech.inspection as inspection} from '../db/schema';
+
 service InspectionService {
     entity InspectionLot as projection on inspectionLots.A_InspectionLot {
-            key InspectionLot,	
-            Material,
-            Batch,
-            Plant,
-            InspectionLotOrigin,
-            GoodsMovementType,
-            SalesOrder,
-            SalesOrderItem,
-            Language,
-    }actions{
-        action printForm() returns String
+        key InspectionLot,	
+        Material,
+        Batch,
+        Plant,
+        InspectionLotOrigin,
+        GoodsMovementType,
+        SalesOrder,
+        SalesOrderItem,
+        Language
+    } actions {
+        action printForm() returns String;
     };
-
 
     entity InspectionCharacteristic as projection on inspectionLots.A_InspectionCharacteristic {
-            key InspectionLot,	
-            InspPlanOperationInternalID,
-            InspectionCharacteristic,
-            InspectionSpecificationText,
-            QuantityUnit,
-            InspectionMethod,
-            InspectionSpecification,
-            InspectorQualification,
-    }
+        key InspectionLot,	
+        InspPlanOperationInternalID,
+        InspectionCharacteristic,
+        InspectionSpecificationText,
+        QuantityUnit,
+        InspectionMethod,
+        InspectionSpecification,
+        InspectorQualification
+    };
 
     entity InspectionOperation as projection on inspectionLots.A_InspectionOperation {
-            key InspectionLot,
-            InspPlanOperationInternalID,
-            OrderInternalBillOfOperations,
-            InspectionOperation
-    }
+        key InspectionLot,
+        InspPlanOperationInternalID,
+        OrderInternalBillOfOperations,
+        InspectionOperation
+    };
 
-    entity InspectionResult as projection on inspectionLots.A_InspectionResult{
-            key InspectionLot,
-            InspPlanOperationInternalID,
-            InspectionCharacteristic,
-            InspectionResultAttribute,
-            InspectionValuationResult,
-            InspectionResultMeanValue,
-            CharacteristicAttributeCode,
-            InspectionStartDate,
-            InspectionEndDate,
-            CreatedByUser,
-            CreationDate,
-            CharacteristicAttributeCodeGrp,
-    }
-    entity InspectionResultValue as projection on inspectionLots.A_InspectionResultValue{
-            key InspectionLot,
-            InspPlanOperationInternalID,
-            InspectionCharacteristic,
-            Inspector,
-            InspectionStartDate,
-            InspectionEndDate,
-            InspectionResultAttribute,
-            InspectionResultMeasuredValue
+    entity InspectionResult as projection on inspectionLots.A_InspectionResult {
+        key InspectionLot,
+        InspPlanOperationInternalID,
+        InspectionCharacteristic,
+        InspectionResultAttribute,
+        InspectionValuationResult,
+        InspectionResultMeanValue,
+        CharacteristicAttributeCode,
+        InspectionStartDate,
+        InspectionEndDate,
+        CreatedByUser,
+        CreationDate,
+        CharacteristicAttributeCodeGrp
+    };
+
+    entity InspectionResultValue as projection on inspectionLots.A_InspectionResultValue {
+        key InspectionLot,
+        InspPlanOperationInternalID,
+        InspectionCharacteristic,
+        Inspector,
+        InspectionStartDate,
+        InspectionEndDate,
+        InspectionResultAttribute,
+        InspectionResultMeasuredValue
+    };
+
+    entity InspectionUsageValue as projection on inspectionLots.A_InspLotUsageDecision {
+        key InspectionLot,
+        InspLotUsageDecisionLevel,
+        InspectionLotQualityScore,
+        InspLotUsageDecisionCatalog,
+        SelectedCodeSetPlant,
+        InspLotUsgeDcsnSelectedSet,
+        InspLotUsageDecisionCodeGroup,
+        InspectionLotUsageDecisionCode,
+        InspLotUsageDecisionValuation,
+        InspLotUsgeDcsnFollowUpAction,
+        InspectionLotUsageDecidedBy,
+        InspLotUsageDecisionTime,
+        InspLotUsgeDcsnChangedTime,
+        InspLotUsgeDcsnHasLongText,
+        ChangedDateTime
     };
 }
-
-
--- srv/sat.js
+++ srv/sat.js
@@ -25,48 +25,57 @@ module.exports = cds.service.impl(async function () {
         return await inspectionLots.run(req.query);
     });
 
-    const { InspectionLot } = this.entities;
-    this.on('printForm', 'InspectionLot', async (req) => {
-        const {
-            InspectionLot, InspectionCharacteristic, InspectionOperation,
-            InspectionResult, InspectionResultValue
-        } = this.entities;
-
-        const inspectionLotId = req.params[0].InspectionLot;
-
-        const lotData = await inspectionLots.run(SELECT.from(InspectionLot).where({ InspectionLot: inspectionLotId }));
-        const charData = await inspectionLots.run(SELECT.from(InspectionCharacteristic).where({ InspectionLot: inspectionLotId }));
-        const opData = await inspectionLots.run(SELECT.from(InspectionOperation).where({ InspectionLot: inspectionLotId }));
-        const resData = await inspectionLots.run(SELECT.from(InspectionResult).where({ InspectionLot: inspectionLotId }));
-        const resValueData = await inspectionLots.run(SELECT.from(InspectionResultValue).where({ InspectionLot: inspectionLotId }));
-
-        const structuredData = {
-            InspectionLotNode: {
-                ...lotData[0],
-                InspectionOperations: opData.map(item => ({
-                    ...item,
-                    InspectionCharacteristics: charData.filter(char => char.InspPlanOperationInternalID === item.InspPlanOperationInternalID),
-                    InspectionResults: resData.filter(res => res.InspPlanOperationInternalID === item.InspPlanOperationInternalID),
-                    InspectionResultValues: resValueData.filter(val => val.InspPlanOperationInternalID === item.InspPlanOperationInternalID)
-                }))
-            }
-        };
-
-        function ensureEmptyTags(obj) {
-            if (Array.isArray(obj)) {
-                return obj.length === 0 ? {} : obj.map(ensureEmptyTags);
-            } else if (typeof obj === 'object' && obj !== null) {
-                return Object.fromEntries(
-                    Object.entries(obj).map(([key, value]) => [key, ensureEmptyTags(value)])
-                );
-            }
-            return obj;
-        }
-
-        const updatedJsonData = ensureEmptyTags(structuredData);
-        const xml = create(updatedJsonData).end({ prettyPrint: true });
-        console.log("Generated XML:", xml);
-
-        return xml;
+    this.on('READ', 'InspectionUsageValue', async (req) => {
+        return await inspectionLots.run(req.query);
     });
+
+    const { InspectionLot } = this.entities;
+
+    this.on('printForm', 'InspectionLot', async (req) => {
+        try {
+            const inspectionLotId = req.params[0].InspectionLot;
+    
+            const lotData = await inspectionLots.run(SELECT.from(InspectionLot).where({ InspectionLot: inspectionLotId }));
+            const charData = await inspectionLots.run(SELECT.from(this.entities.InspectionCharacteristic).where({ InspectionLot: inspectionLotId }));
+            const opData = await inspectionLots.run(SELECT.from(this.entities.InspectionOperation).where({ InspectionLot: inspectionLotId }));
+            const resData = await inspectionLots.run(SELECT.from(this.entities.InspectionResult).where({ InspectionLot: inspectionLotId }));
+            const resValueData = await inspectionLots.run(SELECT.from(this.entities.InspectionResultValue).where({ InspectionLot: inspectionLotId }));
+            const usagevalData = await inspectionLots.run(SELECT.from(this.entities.InspectionUsageValue).where({ InspectionLot: inspectionLotId }));
+    
+            const structuredData = {
+                InspectionLotNode: {
+                    ...lotData[0],
+                    InspectionOperations: opData.map(item => ({
+                        ...item,
+                        InspectionCharacteristics: charData.filter(char => char.InspPlanOperationInternalID === item.InspPlanOperationInternalID),
+                        InspectionResults: resData.filter(res => res.InspPlanOperationInternalID === item.InspPlanOperationInternalID),
+                        InspectionResultValues: resValueData.filter(val => val.InspPlanOperationInternalID === item.InspPlanOperationInternalID),
+                        InspectionUsageValues: usagevalData.filter(val => val.InspectionLot === item.InspectionLot) // Adjusted criteria
+                    }))
+                }
+            };
+    
+            function ensureEmptyTags(obj) {
+                if (Array.isArray(obj)) {
+                    return obj.length === 0 ? {} : obj.map(ensureEmptyTags);
+                } else if (typeof obj === 'object' && obj !== null) {
+                    return Object.fromEntries(
+                        Object.entries(obj).map(([key, value]) => [key, ensureEmptyTags(value)])
+                    );
+                }
+                return obj;
+            }
+    
+            const updatedJsonData = ensureEmptyTags(structuredData);
+            const xml = create(updatedJsonData).end({ prettyPrint: true });
+            console.log("Generated XML:", xml);
+    
+            return xml;
+    
+        } catch (error) {
+            console.error("Error generating XML:", error);
+            req.error(500, "Error generating XML document.");
+        }
+    });
+    
 });
